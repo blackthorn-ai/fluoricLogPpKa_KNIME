3e0d139c003611b7ea83168e2e46bd08e2ed5bd2
PetroPavliukk <ppav@blackthorn.ai>
2024-03-20 16:06:28 +0200

    separate logp/pka nodes

--- my_extension.py
+++ my_extension.py
@@ -8,22 +8,54 @@ from utils import predict_logP, predict_pKa
 
 LOGGER = logging.getLogger(__name__)
 
-@knext.node(name="Fluoriclogppka Node", node_type=knext.NodeType.LEARNER, icon_path="icon.png", category="/")
+@knext.node(name="Fluoric logP", node_type=knext.NodeType.LEARNER, icon_path="icons\chem_icon.png", category="/")
 @knext.input_table(name="Input SMILES Data", description="Table with SMILES in 'string' of 'SMI' format")
-@knext.output_table(name="Output Data", description="Table with predicted pKa and logP values")
-class FluoriclogppkaNode:
-    """Tool for predicting logP, pKa using molecules SMILES.
+@knext.output_table(name="Output Data", description="Table with predicted logP values")
+class Fluoriclogp:
+    """Node for predicting logP using molecules SMILES.
     For predicting are used a lot of generated molecule features, such as dihedral angle, molecular weight and volume, amount of Carbon atoms etc.
-    All this molecule features used for predicting two values - pKa, logP using H2O models.
+    All this molecule features used for predicting logP values using H2O models.
     """
-    
-    boolean_pka_param = knext.BoolParameter("Predict pKa", "Checkbox parameter in case you want to predict the pKa value or not", True)
-    boolean_logp_param = knext.BoolParameter("Predict logP", "Checkbox parameter in case you want to predict the logP value or not", True)
-
 
     def configure(self, configure_context, input_schema_1):
         
         input_schema_1 = input_schema_1.append(knext.Column(knext.double(), "logP"))
+
+        return input_schema_1
+
+ 
+    def execute(self, exec_context, input_1):
+        input_pandas = input_1.to_pandas()
+
+        predicted_logPs = []
+        for _, row in input_pandas.iterrows():
+            if pd.isnull(row['SMILES']):
+                raise ValueError("SMILES cannot be NaN")
+
+            SMILES = row['SMILES']
+            
+            predicted_logP = predict_logP(SMILES=SMILES)
+            predicted_logPs.append(predicted_logP)
+
+        output_df = pd.DataFrame({
+            'SMILES': input_pandas['SMILES'],
+            'logP': predicted_logPs,
+        })
+        
+        return knext.Table.from_pandas(output_df)
+
+
+@knext.node(name="Fluoric pKa", node_type=knext.NodeType.LEARNER, icon_path="icons\chem_icon.png", category="/")
+@knext.input_table(name="Input SMILES Data", description="Table with SMILES in 'string' of 'SMI' format")
+@knext.output_table(name="Output Data", description="Table with predicted pKa values")
+class Fluoricpka:
+    """Node for predicting pKa using molecules SMILES.
+    For predicting are used a lot of generated molecule features, such as dihedral angle, molecular weight and volume, amount of Carbon atoms etc.
+    All this molecule features used for predicting pKa values using H2O models.
+    """
+
+    def configure(self, configure_context, input_schema_1):
+        
         input_schema_1 = input_schema_1.append(knext.Column(knext.double(), "pKa"))
 
         return input_schema_1
@@ -40,18 +72,13 @@ class FluoriclogppkaNode:
 
             SMILES = row['SMILES']
 
-            if self.boolean_pka_param is True:
-                predicted_pKa = predict_pKa(SMILES=SMILES)
-                predicted_pKas.append(predicted_pKa)
-            
-            if self.boolean_logp_param is True:
-                predicted_logP = predict_logP(SMILES=SMILES)
-                predicted_logPs.append(predicted_logP)
+            predicted_pKa = predict_pKa(SMILES=SMILES)
+            predicted_pKas.append(predicted_pKa)
+
 
         output_df = pd.DataFrame({
             'SMILES': input_pandas['SMILES'],
-            'logP': predicted_logPs if self.boolean_logp_param else [math.nan for i in range(len(input_pandas['SMILES']))],
-            'pKa': predicted_pKas if self.boolean_pka_param else [math.nan for i in range(len(input_pandas['SMILES']))]
+            'pKa': predicted_pKas,
         })
         
         return knext.Table.from_pandas(output_df)
